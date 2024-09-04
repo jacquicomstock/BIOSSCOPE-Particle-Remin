f8cdd2d8216093e22d89585cde5039afcfc46939
Jacqui Comstock <41028747+jacquicomstock@users.noreply.github.com>
2024-09-04 16:54:01 -0700

    Update calculate_z_score.R

--- statistics_and_analyses/calculate_z_score.R
+++ statistics_and_analyses/calculate_z_score.R
@@ -4,7 +4,6 @@ ASV <- PR[,10:ncol(PR)]
 meta <- PR[,1:9]
 relabund.df <- as.data.frame(t((ASV/100)))
 
-
 ASV_cull <- relabund.df %>%
   filter(rowSums(. > .01) >= 2)
 
@@ -19,10 +18,6 @@ result <- apply(ASV_cull, 2, zscore)
 
 #convert result back into a dataframe
 result_df <- as.data.frame(t(result))
-
-#target and remove contaminant DNA (ASVs #1,2,3,5,8)
-#rows_to_remove <- c(1, 2, 3, 5, 8)
-#filtered_data <- as.data.frame(t(result_df[-rows_to_remove, ]))
 
 # Calculate averages by group
 tRA_meta <- as.data.frame(cbind(meta$Treatment_Fraction_Timepoint, result_df))
